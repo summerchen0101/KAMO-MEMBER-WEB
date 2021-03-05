0da71d10885b81421a1abebb682b27685e5459d9
Summer Chen <mp4bj6ruby@gmail.com>
2021-03-05 10:35:36 +0800

    error shooting

--- components/FieldValidateMessage.tsx
+++ components/FieldValidateMessage.tsx
@@ -1,17 +1,16 @@
-import { FormHelperText } from '@chakra-ui/form-control'
 import Icon from '@chakra-ui/icon'
-import { Box, HStack, Text } from '@chakra-ui/layout'
+import { Box } from '@chakra-ui/layout'
 import React from 'react'
 import { FieldError } from 'react-hook-form/dist/types'
-import { HiInformationCircle, HiOutlineInformationCircle } from 'react-icons/hi'
+import { HiInformationCircle } from 'react-icons/hi'
 
 const FieldValidateMessage: React.FC<{ error?: FieldError }> = ({ error }) => {
   if (!error) return <></>
   return (
-    <FormHelperText color="red" fontSize="14px">
+    <Box className="text-red" fontSize="14px">
       <Icon as={HiInformationCircle} mr="5px" />
       {error.message}
-    </FormHelperText>
+    </Box>
   )
 }
 
